d3666e19d46bd5bdd7054d29336cc3da68cb9932
rom <mozzieisme@gmail.com>
2021-11-03 21:38:39 -0400

    Modified

--- play.js
+++ play.js
@@ -1,4 +1,29 @@
 const { connect } = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+
+
+setupInput();
+
 console.log("Connecting ...");
 connect();
